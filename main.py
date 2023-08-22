3153755f19e41a39416f1039f618cc24c9b9197a
Sumayo Rage <sumayo@Sumayos-MacBook-Air.local>
2023-08-22 13:28:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+#jakolaskukone
+
+num1 = int(input("Anna jaettava luku? "))
+num2 = int(input("Anna jakaja: "))
+result = num1 / num2
+# tyyppimuunnos "f-stringillä"
+print(f"Jakolaskun tulos: {result:.3f}")
